be035e48a407305994db03b1fccc641fe27b2fe5
Aaron Schaffer <40671448+ajschaf@users.noreply.github.com>
2023-06-13 10:51:23 -0400

    Update rsync-latest.sh

--- rsync-latest.sh
+++ rsync-latest.sh
@@ -1,2 +1,2 @@
 rsync -az --progress --exclude '.svn' --exclude 'tmp/' \
-    --relative data.theyworkforyou.com::parldata/scrapedxml/regmem .
+    --relative data.theyworkforyou.com::pwdata/scrapedxml/regmem .
